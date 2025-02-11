cde4b178e564944ce75ed1164df12d16c7f684ee
Neto <maciasf@perspective.com.mx>
2025-02-11 16:32:45 -0600

    docs: add comments

--- File.py
+++ File.py
@@ -1,12 +1,27 @@
 class File:
-    def __init__(self,open_file,output=False):
+    def __init__(self, open_file):
+        """Initializes a File object by reading the contents of the provided file.
+
+        Args:
+            open_file: A file object or context manager that supports the `with` statement. This is typically an instance of `OpenFile`.
+        
+        Attributes:
+            file (list): A list containing the lines of the file.
+            files_lines (int): The number of lines in the file.
+        """
         file = self.__read_file(open_file)
         self.file = file
         self.files_lines = len(file)
-        
-    def __read_file(self,open_file):
-        """Read open file content using OpenFile."""
+
+    def __read_file(self, open_file):
+        """Reads the contents of the file and returns its lines as a list.
+
+        Args:
+            open_file: A file object or context manager that supports the `with` statement. This is typically an instance of `OpenFile`.
+
+        Returns:
+            list: A list of strings, where each string represents a line in the file.
+        """
         with open_file as archivo:
             return archivo.readlines()
-        
         
--- OpenFile.py
+++ OpenFile.py
@@ -1,14 +1,33 @@
 class OpenFile:
     def __init__(self, file_path, mode='r'):
+        """Initializes the context for opening a file.
+
+        Args:
+            file_path (str): The path of the file to be opened.
+            mode (str, optional): The mode in which the file is opened. Defaults to 'r' (read).
+        """
         self.file_path = file_path
         self.mode = mode
         self.file = None
 
     def __enter__(self):
-        """Open file and return object file."""
+        """Opens the file and returns it for use within the `with` block.
+
+        Returns:
+            file: The opened file object.
+        """
         self.file = open(self.file_path, self.mode)
         return self.file
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        """When exit the context close file automaticlly."""
+        """Closes the file when exiting the `with` block and raise and exception if error.
+
+        Args:
+            exc_type (type): The type of exception if one occurred.
+            exc_val (Exception): The exception instance if one occurred.
+            exc_tb (traceback): The traceback object if an exception occurred.
+        """
         self.file.close()
+
+        if exc_type is not None:
+            print(f"An exception occurred: {exc_type} - {exc_val}")
